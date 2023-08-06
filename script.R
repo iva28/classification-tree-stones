ff866037b00a30561ec8a69e3a7f16753871dd2a
iva28 <iva.hurt574@gmail.com>
2023-08-06 22:58:44 +0200

    Classification tree

---
+++ script.R
@@ -0,0 +1,31 @@
+#loading "The Rolling Stones" dataset
+dataset <- read.csv('stones_analysis.csv',stringsAsFactors = TRUE)
+
+#examining the structure of dataset
+str(dataset)
+#examining the summary of dataset
+summary(dataset)       
+
+#making outcome variable "Award"
+unique(dataset$Certification)
+str(dataset$Certification)
+
+dataset$Award <- factor(ifelse(test = dataset$Certification != "-", yes = "Yes", no = "No"), levels = c("Yes", "No"))
+prop.table(table(dataset$Award))
+
+#deleting Certification variable that was used for creating Award variable
+dataset$Certification <- NULL
+
+length(unique(dataset$Title))
+#each song title is unique so it will be excluded from model
+dataset$Title <- NULL 
+
+#checking missing values in dataset
+apply(dataset, MARGIN = 2,FUN = function(x) sum(is.na(x)))
+#the aren't any NA values
+apply(dataset, MARGIN = 2,FUN = function(x) sum(x == "", na.rm = T))
+#there are not any "" values
+apply(dataset, MARGIN = 2,FUN = function(x) sum(x == " ", na.rm = T))
+#the are not any " " values
+
+
